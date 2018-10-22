8405cf5fac92f5fd600da2ee01879a0918d4f549
Pack Fan 1031 <ocskier@gmail.com>
2018-10-22 00:46:01 -0400

    styling on mobile

--- clickygame/src/components/StillCard/StillCard.js
+++ clickygame/src/components/StillCard/StillCard.js
@@ -4,7 +4,7 @@ import "./StillCard.css";
 const winner = "./check-mark-1292787_1280.png"
 
 const StillCard = props => (
-  <div className="card small">
+  <div className="card z-depth-5 small">
     <button style={{padding: 0,backgroundColor: "transparent"}} onClick={props.hidden || props.disableAll ? () => null : (e) => props.cardClickHandler(e,props.id)}>
       <div className="card-image" style={{maxHeight: "100%"}}>
         <img style={{width: "100%", backgroundSize: "cover",backgroundPosition: "50%",backgroundClip: "content-box",height: "210px"}} alt={props.name} src={props.hidden ? winner : props.image} />
