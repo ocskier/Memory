3e629d578bd684ee4d191b1a7f01c7a566248fea
Jon Jackson <ocskier@gmail.com>
2019-09-13 17:31:08 -0400

    working on css border bug on mobile

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,10 +2,46 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className={props.matched ? "card small animated heartBeat" : props.allDisabled ? "card small animated flash" : "card small animated flipInY"}>
-    <div className="card-image" style={{maxHeight: "100%"}}>
-      <img className="friendImg" style={{width: "100%",maxHeight: "280px",paddingTop: "40px",backgroundSize: "cover",backgroundPosition: "50%",backgroundClip: "content-box"}} alt={props.name} src={props.image} />
-      <span className="card-title black-text" style={{top: 0,padding: "12px",width: "100%",textAlign: "center",fontSize:"1rem"}}>{props.name}</span>
+  <div
+    className={
+      props.matched
+        ? "card small animated heartBeat"
+        : props.allDisabled
+        ? "card small animated flash"
+        : "card small animated flipInY"
+    }
+  >
+    <div
+      className="card-image"
+      style={{ borderRadius: "10px !important", maxHeight: "100%" }}
+    >
+      <img
+        className="friendImg"
+        style={{
+          width: "100%",
+          maxHeight: "280px",
+          paddingTop: "40px",
+          backgroundSize: "cover",
+          backgroundPosition: "50%",
+          backgroundClip: "content-box",
+          borderRadius: "10px !important"
+        }}
+        alt={props.name}
+        src={props.image}
+      />
+      <span
+        className="card-title black-text"
+        style={{
+          top: 0,
+          borderRadius: "10px !important",
+          padding: "12px",
+          width: "100%",
+          textAlign: "center",
+          fontSize: "1rem"
+        }}
+      >
+        {props.name}
+      </span>
     </div>
   </div>
 );
--- src/components/StillCard/StillCard.js
+++ src/components/StillCard/StillCard.js
@@ -1,17 +1,48 @@
 import React from "react";
 import "./StillCard.css";
 
-const winner = "./check-mark-1292787_1280.png"
+const winner = "./check-mark-1292787_1280.png";
 
 const StillCard = props => (
-  <div style={{background: `url(${props.hidden ? winner : props.image}) no-repeat center center fixed`}} className={props.reset ? "card z-depth-5 small animated shake" : "card z-depth-5 small"} >
-    <button style={{padding: 0,backgroundColor: "transparent"}} onClick={props.hidden || props.disableAll ? () => null : (e) => props.cardClickHandler(e,props.id)}>
-      <div className="card-image" style={{maxHeight: "100%"}} >
-        {/* <img className="stillImg" style={{width: "100%", backgroundSize: "cover",backgroundPosition: "50%",backgroundClip: "content-box",height: "210px"}} alt={props.name} src={props.hidden ? winner : props.image} /> */}
+  <div
+    className={
+      props.reset
+        ? "card z-depth-5 small animated shake"
+        : "card z-depth-5 small"
+    }
+  >
+    <button
+      style={{
+        borderRadius: "10px !important",
+        padding: 0,
+        backgroundColor: "transparent"
+      }}
+      onClick={
+        props.hidden || props.disableAll
+          ? () => null
+          : e => props.cardClickHandler(e, props.id)
+      }
+    >
+      <div
+        className="card-image"
+        style={{ maxHeight: "100%", borderRadius: "10px !important" }}
+      >
+        <img
+          className="stillImg"
+          style={{
+            borderRadius: "10px !important",
+            width: "100%",
+            backgroundSize: "cover",
+            backgroundPosition: "50%",
+            backgroundClip: "content-box",
+            height: "210px"
+          }}
+          alt={props.name}
+          src={props.hidden ? winner : props.image}
+        />
       </div>
     </button>
   </div>
 );
 
 export default StillCard;
-
